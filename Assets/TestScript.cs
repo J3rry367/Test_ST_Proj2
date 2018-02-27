cb78e29e38f0c2a777c979b545c9f02e6d1e254c
Jerry <jerry.johnson367@gmail.com>
2018-02-27 05:23:00 -0800

    Added something to Script

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -7,6 +7,7 @@ public class TestScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("I'm a script Kitty. Meow!");
+        print("Something Changed");
 	}
 	
 	// Update is called once per frame
